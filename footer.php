f090fbb5f237f46739af63ec7c933ffffe4cf45f
isabelaranguren <isabelarangurenl01@gmail.com>
2024-02-23 15:28:51 -0600

    Update footer

--- footer.php
+++ footer.php
@@ -1,159 +1,189 @@
-
-
-			<!-- FOOTER-1
+<!-- FOOTER-1
 			============================================= -->
-			<footer id="footer-1" class="bg-color-01 footer division">
-				<div class="container">
+<footer id="footer-1" class="bg-color-01 footer division">
+    <div class="container">
 
 
-					<!-- FOOTER CONTENT -->
-					<div class="row">
+        <!-- FOOTER CONTENT -->
+        <div class="row">
 
 
-						<!-- FOOTER INFO -->
-						<div class="col-md-6 col-lg-4 col-xl-5">
-							<div class="footer-info mb-40">
+            <!-- FOOTER INFO -->
+            <div class="col-md-6 col-lg-4 col-xl-5">
+                <div class="footer-info mb-40">
 
-								<!-- For Retina Ready displays take a image with double the amount of pixels that your image will be 
+                    <!-- For Retina Ready displays take a image with double the amount of pixels that your image will be 
 								displayed (e.g 416 x 120  pixels) -->
-								<img src="<?php echo get_template_directory_uri().'/images/logo-01.png';?>" width="204" height="60" alt="footer-logo">
+                    <img src="<?php echo get_template_directory_uri().'/images/logo-01.png';?>" width="204" height="60"
+                        alt="footer-logo">
 
-								<!-- Footer Contacts -->
-								<div class="footer-contacts txt-color-05 mt-25">
+                    <!-- Footer Contacts -->
+                    <div class="footer-contacts txt-color-05 mt-25">
 
-									<!-- Address -->
-									<p>121 King Street, Melbourne,</p> 
-									<p>Victoria 3000 Australia</p>
+                        <!-- Address -->
+                        <p>2400 Augusta Dr #110</p>
+                        <p>Houston, TX 77057</p>
 
-									<!-- Email -->
-									<p class="foo-email">E: <a href="mailto:yourdomain@mail.com">hello@yourdomain.com</a></p>
+                        <!-- Email -->
+                        <p class="foo-email">E: <a
+                                href="mailto:contact@lashextensionsbyfiona.com">contact@lashextensionsbyfiona.com</a>
+                        </p>
 
-									<!-- Phone -->
-									<p>P: +12 9 8765 4321</p>
+                        <!-- Phone -->
+                        <p>(832) 866-7665</p>
 
-								</div>
+                    </div>
 
-							</div>	
-						</div>	
-
-
-						<!-- FOOTER LINKS -->
-						<div class="col-md-3 col-lg-2">
-							<div class="footer-links mb-40">
-							
-								<!-- Title -->
-								<h6 class="h6-lg txt-color-01">About Us</h6>
-
-								<!-- Footer Links -->
-								<ul class="clearfix txt-color-05">
-									<li><p><a href="demo-1.html#">About Us</a></p></li>		
-									<li><p><a href="demo-1.html#">Contact Us</a></p></li>	
-									<li><p><a href="demo-1.html#">Career</a></p></li>	
-									<li><p><a href="demo-1.html#">Advertising</a></p></li>
-									<li><p><a href="demo-1.html#">Reviews</a></p></li>	
-								</ul>
-
-							</div>
-						</div>
+                </div>
+            </div>
 
 
-						<!-- FOOTER LINKS -->
-						<div class="col-md-3 col-lg-2">
-							<div class="footer-links mb-40">
-							
-								<!-- Title -->
-								<h6 class="h6-lg txt-color-01">Userful Links</h6>
+            <!-- FOOTER LINKS -->
+            <div class="col-md-3 col-lg-2">
+                <div class="footer-links mb-40">
 
-								<!-- Footer List -->
-								<ul class="clearfix txt-color-05">									
-									<li><p><a href="demo-1.html#">Location</a></p></li>																	
-									<li><p><a href="demo-1.html#">Help & FAQs</a></p></li>
-									<li><p><a href="demo-1.html#">Appointments</a></p></li>
-									<li><p><a href="demo-1.html#">Membership</a></p></li>	
-									<li><p><a href="demo-1.html#">Gift Cards</a></p></li>						
-								</ul>
+                    <!-- Title -->
+                    <h6 class="h6-lg txt-color-01">About Us</h6>
 
-							</div>
-						</div>
+                    <!-- Footer Links -->
+                    <ul class="clearfix txt-color-05">
+                        <li>
+                            <p><a href="demo-1.html#">About Us</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Contact Us</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Career</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Advertising</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Reviews</a></p>
+                        </li>
+                    </ul>
 
-
-						<!-- FOOTER NEWSLETTER FORM -->
-						<div class="col-md-8 col-lg-4 col-xl-3">
-							<div class="footer-form mb-20">
-
-								<!-- Title -->
-								<h6 class="h6-lg txt-color-01">Subscribe Us:</h6>
-
-								<!-- Text -->	
-								<p class="txt-color-05 mb-20">Stay up to date with our latest news and our new products</p>
-
-								<!-- Newsletter Form Input -->
-								<form class="newsletter-form">
-											
-									<div class="input-group">
-										<input type="email" class="form-control" placeholder="Email Address" required id="s-email">								
-										<span class="input-group-btn">
-											<button type="submit" class="btn txt-color-06">
-												<i class="far fa-arrow-alt-circle-right"></i>
-											</button>
-										</span>
-									</div>
-
-									<!-- Newsletter Form Notification -->		
-									<label for="s-email" class="form-notification"></label>
-												
-								</form>
-														
-							</div>
-						</div>	<!-- END FOOTER NEWSLETTER FORM -->
+                </div>
+            </div>
 
 
-					</div>	  <!-- END FOOTER CONTENT -->
+            <!-- FOOTER LINKS -->
+            <div class="col-md-3 col-lg-2">
+                <div class="footer-links mb-40">
+
+                    <!-- Title -->
+                    <h6 class="h6-lg txt-color-01">Userful Links</h6>
+
+                    <!-- Footer List -->
+                    <ul class="clearfix txt-color-05">
+                        <li>
+                            <p><a href="demo-1.html#">Location</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Help & FAQs</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Appointments</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Membership</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#">Gift Cards</a></p>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
 
 
-					<!-- BOTTOM FOOTER -->
-					<div class="bottom-footer txt-color-05">
-						<div class="row d-flex align-items-center">
+            <!-- FOOTER NEWSLETTER FORM -->
+            <div class="col-md-8 col-lg-4 col-xl-3">
+                <div class="footer-form mb-20">
+
+                    <!-- Title -->
+                    <h6 class="h6-lg txt-color-01">Subscribe Us:</h6>
+
+                    <!-- Text -->
+                    <p class="txt-color-05 mb-20">Stay up to date with our latest news and our new products</p>
+
+                    <!-- Newsletter Form Input -->
+                    <form class="newsletter-form">
+
+                        <div class="input-group">
+                            <input type="email" class="form-control" placeholder="Email Address" required id="s-email">
+                            <span class="input-group-btn">
+                                <button type="submit" class="btn txt-color-06">
+                                    <i class="far fa-arrow-alt-circle-right"></i>
+                                </button>
+                            </span>
+                        </div>
+
+                        <!-- Newsletter Form Notification -->
+                        <label for="s-email" class="form-notification"></label>
+
+                    </form>
+
+                </div>
+            </div> <!-- END FOOTER NEWSLETTER FORM -->
 
 
-							<!-- FOOTER COPYRIGHT -->
-							<div class="col-lg-6">
-								<div class="footer-copyright">
-									<p>&copy; 2020 Spa Magic. All Rights Reserved</p>
-								</div>
-							</div>
+        </div> <!-- END FOOTER CONTENT -->
 
 
-							<!-- BOTTOM FOOTER LINKS -->
-							<div class="col-lg-6">
-								<ul class="bottom-footer-list text-right clearfix">
-									<li><p class="first-list-link"><a href="demo-1.html#"><i class="fab fa-facebook-f"></i> Facebook</a></p></li>	
-									<li><p><a href="demo-1.html#"><i class="fab fa-twitter"></i> Twitter</a></p></li>
-									<li><p><a href="demo-1.html#"><i class="fab fa-instagram"></i> Instagram</a></p></li>
-									<li><p class="last-li"><a href="demo-1.html#"><i class="fab fa-yelp"></i> Yelp</a></p></li>
-								</ul>
-							</div>
+        <!-- BOTTOM FOOTER -->
+        <div class="bottom-footer txt-color-05">
+            <div class="row d-flex align-items-center">
 
 
-						</div>  <!-- End row -->
-					</div>	<!-- END BOTTOM FOOTER -->
+                <!-- FOOTER COPYRIGHT -->
+                <div class="col-lg-6">
+                    <div class="footer-copyright">
+                        <p>&copy; 2020 Spa Magic. All Rights Reserved</p>
+                    </div>
+                </div>
 
 
-				</div>	   <!-- End container -->										
-			</footer>	<!-- END FOOTER-1 -->
+                <!-- BOTTOM FOOTER LINKS -->
+                <div class="col-lg-6">
+                    <ul class="bottom-footer-list text-right clearfix">
+                        <li>
+                            <p class="first-list-link"><a href="demo-1.html#"><i class="fab fa-facebook-f"></i>
+                                    Facebook</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#"><i class="fab fa-twitter"></i> Twitter</a></p>
+                        </li>
+                        <li>
+                            <p><a href="demo-1.html#"><i class="fab fa-instagram"></i> Instagram</a></p>
+                        </li>
+                        <li>
+                            <p class="last-li"><a href="demo-1.html#"><i class="fab fa-yelp"></i> Yelp</a></p>
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div> <!-- End row -->
+        </div> <!-- END BOTTOM FOOTER -->
+
+
+    </div> <!-- End container -->
+</footer> <!-- END FOOTER-1 -->
 
 
 
 
-		</div>	<!-- END PAGE CONTENT -->
+</div> <!-- END PAGE CONTENT -->
 
 
 
-        <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 
-	</body>
+</body>
 
 
 
-</html>	
+</html>
