978e31c3de32b2b3ecdeab82607f33ac5348763d
Yash Gandhi <gyash557@gmail.com>
2024-02-27 11:36:08 -0600

    changed footer

--- template-face-service.php
+++ template-face-service.php
@@ -14,20 +14,20 @@ get_header();
 <div class="container">
     <div class="row">
         <!-- Content Table -->
-        <div class="col-lg-4 order-lg-2">
-            <div class="sidebar bg-color-05 mt-100 ml-0 shadow">
+        <div class="col-lg-4 order-lg-1">
+            <div class="sidebar bg-color-05 mt-100  shadow">
                 <h6 class="text-center h6-lg txt-color-01 p-4">Content Table</h6>
                 <ul class="tracking-wider ml-20 font-weight-bold">
                     <li class="list-itemed p-2"><a href="#section1">The Art of Eyebrow Waxing</a></li>
                     <li class="list-itemed p-2"><a href="#section2">How Does Eyebrow Waxing Work? </a></li>
-                    <li class="list-itemed p-2">Waxing vs. Threading: Which is Better?</li>
+                    <li class="list-itemed p-2"><a href="#section3">Waxing vs. Threading: Which is Better?</a></li>
                     <li class="list-itemed p-2">The Eyebrow Waxing Experience at Lash Extensions by Fiona</li>
                 </ul>
             </div>
         </div>
         <!-- Heading Text -->
         <div class="col-lg-8 order-lg-1">
-            <div class="heading ">
+            <div class="heading">
                 <p><span class="font-weight-bold fs-10">At Lash Extensions by Fiona,</span> we believe that beauty is in the details. This is why our expertise extends beyond lashes. In our mission to provide comprehensive solutions to all of your eye-enhancing demands, we are pleased to announce one of our most popular services:</p><br />
                 <p>Our eyebrow waxing service, created with the same meticulous attention to detail that has made our lash extensions the talk of the town, is more than just removal; it's about sculpting, defining, and perfecting your brows to complement your natural beauty and the stunning appearance of your lash extensions.
                     Our team of experts, trained in the most recent techniques and trends, guarantees that each brow is perfectly formed, accentuating your face's natural arch and symmetry. We realize the importance of precision in this delicate area, which is why we only use high-quality waxes and calming agents to provide a comfortable and satisfying experience. Elevate your beauty regimen with Lash Extensions by Fiona, which give your brows the attention they need while smoothly integrating with your magnificent lash extensions for a flawless look.</p>
@@ -38,25 +38,25 @@ get_header();
 
 
 <section id="section1">
-<div class="container">
-    <div class="row">
-        <div class="col-lg-8 col-md-4 mt-20">
-            <h3 class="h3-lg">The Art of Eyebrow Waxing</h3>
-            <p class="p-4 ml-left">Waxing is a precise and delicate technique that involves not just skill and precision, but also a sense of beauty and aesthetics. At the heart of this art is the idea that brow waxing is more than just a cosmetic process; it's a form of self-expression, a means to highlight your inherent beauty and traits while allowing your individuality to shine through. Waxing, as an art form, extends beyond the basic removal of undesirable hair. It entails understanding the natural curve and contour of the brows to achieve a look that complements the individual's face features. Each stroke, each draw of wax, is executed with the accuracy of a painter's brush, with the goal of shaping the brows into a masterpiece that compliments the individual beauty of the face they frame. The goal is to achieve the ideal balance of thickness and arch, length and curve, so that the brows convey the appropriate expression, whether it's one of elegance, youthfulness, or strong fierceness.</p>
-        
-        <ul class="text-left tracking-wider custom-bullets"> 
-            <li class="list-item">Furthermore, The art of waxing considers the health and comfort of the skin. Pre- and post-waxing care are essential components of the process, guaranteeing that the skin is not only smooth and hair-free, but also moisturized and protected. High-quality waxes, calming chemicals, and precise methods are used to reduce discomfort and increase satisfaction.</li><br/>
-            <li class="list-item">In essence, the art of waxing celebrates beauty in its most natural and improved form. It's about creating an appearance that is not just visible but also feels good, boosting the wearer's confidence and sense of self. At Lash Extensions by Fiona, this skill is executed with utmost attention and enthusiasm, ensuring that every client departs with not only exquisite brows but also a deeper appreciation for the craftsmanship involved in creating them.</li>
-        </ul>
-        
-        </div>
-        <div class="col-lg-4 col-md-8 mt-20">
-            <div class="image-container">
-                <img src="<?php echo get_template_directory_uri(). '/images/gallery/img-1.jpg'; ?> " alt="Image">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-4 mt-20 ol-1 p-2 ">
+                <h3 class="h3-lg ">The Art of Eyebrow Waxing</h3>
+                <p class="p-4 ml-left">Waxing is a precise and delicate technique that involves not just skill and precision, but also a sense of beauty and aesthetics. At the heart of this art is the idea that brow waxing is more than just a cosmetic process; it's a form of self-expression, a means to highlight your inherent beauty and traits while allowing your individuality to shine through. Waxing, as an art form, extends beyond the basic removal of undesirable hair. It entails understanding the natural curve and contour of the brows to achieve a look that complements the individual's face features. Each stroke, each draw of wax, is executed with the accuracy of a painter's brush, with the goal of shaping the brows into a masterpiece that compliments the individual beauty of the face they frame. The goal is to achieve the ideal balance of thickness and arch, length and curve, so that the brows convey the appropriate expression, whether it's one of elegance, youthfulness, or strong fierceness.</p>
+
+                <ul class="text-left tracking-wider custom-bullets">
+                    <li class="list-item">Furthermore, The art of waxing considers the health and comfort of the skin. Pre- and post-waxing care are essential components of the process, guaranteeing that the skin is not only smooth and hair-free, but also moisturized and protected. High-quality waxes, calming chemicals, and precise methods are used to reduce discomfort and increase satisfaction.</li><br />
+                    <li class="list-item">In essence, the art of waxing celebrates beauty in its most natural and improved form. It's about creating an appearance that is not just visible but also feels good, boosting the wearer's confidence and sense of self. At Lash Extensions by Fiona, this skill is executed with utmost attention and enthusiasm, ensuring that every client departs with not only exquisite brows but also a deeper appreciation for the craftsmanship involved in creating them.</li>
+                </ul>
+
+            </div>
+            <div class="col-lg-4 col-md-8 mt-20">
+                <div class="image-container">
+                    <img src="<?php echo get_template_directory_uri() . '/images/gallery/img-1.jpg'; ?> " alt="Image">
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
@@ -69,25 +69,50 @@ get_header();
         <div class="row">
             <div class="col-lg-6">
                 <div class="img-container">
-                    <img src="<?php echo get_template_directory_uri(). '/images/gallery/img-2.jpg';?>" alt="Image" class="img-fluid">
+                    <img src="<?php echo get_template_directory_uri() . '/images/gallery/img-2.jpg'; ?>" alt="Image" class="img-fluid">
                 </div>
             </div>
             <div class="col-lg-6 d-flex pt-40 left-text">
                 <div class=" txt-container">
                     <h4 class="  text-black font-weight-bold mb-4 fs-11 ">How Does Eyebrow Waxing Work</h4>
-                    <p class="text-dark font-italic ">Eyebrow waxing is a precise and efficient method of shaping and defining brows that provides immediacy and precision unrivaled by plucking or threading. </p><br/>
+                    <p class="text-dark font-italic ">Eyebrow waxing is a precise and efficient method of shaping and defining brows that provides immediacy and precision unrivaled by plucking or threading. </p><br />
                     <ul class="text-left tracking-wider custom-bullet text-black ">
                         <li>The procedure begins with applying a small layer of warm wax to the areas surrounding the brows, which targets undesirable strands while retaining the natural contour and arch. </li>
                         <li>The wax clings to the hairs rather than the skin, resulting in a more comfortable and smoother process.Once applied, a cloth or paper strip is pressed against the wax. The strip is taken away with a quick motion, eliminating both the wax and the undesirable hair from the roots. </li>
                         <li>This procedure provides a clear, precise line and might better define the brows than other hair removal methods. Following the waxing, a soothing lotion can be used to relax the skin and reduce redness, leaving the brows flawlessly groomed and gracefully defined. </li>
-                    </ul><br/>
+                    </ul><br />
 
-                    <span > At Lash Extensions by Fiona, this method is taken to an art form, resulting in stunning </span><br/> <br/>
+                    <span> At Lash Extensions by Fiona, this method is taken to an art form, resulting in stunning </span><br /> <br />
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<section id="section3">
+    <div class="container pt-100">
+        <div class="row  ">
+            <div class="col-lg-8 col-md-4 mt-20 text-left-content ">
+                <h4 class="h4-lg">Waxing vs. Threading: Which is Better?</h4>
+                <p class="p-4 ml-lt font-italic">Waxing vs. Threading: The dispute over these two popular eyebrow shaping techniques is as heated as it is ongoing. Each has devoted adherents and undeniable benefits, but when it comes to the specifics, waxing is frequently the preferred option for those looking for efficiency, longer-lasting effects, and a touch of elegance in their beauty routine.</p>
+
+                <ul class="text-left tracking-wider custom-point ">
+                    <li class="list-item">Waxing is an extremely precise and clean method of hair removal. Unlike threading, which might miss finer hairs due to the thread's grip restrictions, waxing removes even the smallest, most resistant strands from the root. This thorough cleaning not only leads to a cleaner, more defined brow shape, but it also increases the time between treatments, as waxing often causes slower and finer hair regrowth over time.</li><br />
+                    <li class="list-item">Furthermore, waxing may be perceived as a more comfortable procedure, especially for people with sensitive skin. The fast, single motion of wax removal causes less aggravation than the repeated twists and pulls associated with threading. Advanced wax compositions used in professional settings, such as Lash Extensions by Fiona, are also designed to be mild on the skin, lowering the risk of redness and irritation and making waxing a viable option for a wider range of skin types.</li><br />
+                    <li class="list-item">Waxing is a holistic beauty therapy that goes beyond simply removing hair. Clients leave with perfectly shaped brows as well as a small pampering session that leaves their skin soft and revitalized. While threading has its advantages in brow upkeep, waxing stands out for individuals looking for a more comprehensive, less frequent, and indulgent beauty care regimen.</li><br />
+                </ul>&nbsp;
+
+            </div>
+            <div class="col-lg-4 col-md-8 mt-20 order-lg-2">
+                <div class="img-contain">
+                    <img src="<?php echo get_template_directory_uri() . '/images/gallery/img-1.jpg'; ?> " alt="Image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
 
 
 
