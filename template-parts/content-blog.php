19eccc6589d74945d1e016f82340b279561372a7
isabelaranguren <isabelarangurenl01@gmail.com>
2024-02-27 10:13:05 -0600

    Frontpage images and remove unused sections

---
+++ template-parts/content-blog.php
@@ -0,0 +1,143 @@
+<section id="blog-1" class="bg-color-01 wide-60 blog-section division">
+    <div class="container">
+
+
+        <!-- SECTION TITLE -->
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="section-title mb-60 text-center">
+
+                    <!-- Title 	-->
+                    <h3 class="h3-xl txt-color-01">Blog/h3>
+
+                        <!-- Text -->
+                        <p class="p-lg txt-color-05">Aliquam a augue suscipit, luctus neque purus ipsum neque undo dolor
+                            primis libero tempus, blandit a cursus varius at magna tempor
+                        </p>
+
+                </div>
+            </div>
+        </div>
+
+
+        <!-- BLOG POSTS HOLDER -->
+        <div class="row">
+
+
+            <!-- BLOG POST #1 -->
+            <div class="col-md-6 col-lg-4">
+                <div class="blog-post">
+
+                    <!-- BLOG POST IMAGE -->
+                    <div class="blog-post-img">
+                        <img class="img-fluid"
+                            src="<?php echo get_template_directory_uri() . '/images/blog/img-01.jpg'; ?>"
+                            alt="blog-post-image" />
+                    </div>
+
+                    <!-- BLOG POST TITLE -->
+                    <div class="blog-post-txt">
+
+                        <!-- Post Data -->
+                        <span class="txt-color-06">Skincare</span>
+
+                        <!-- Post Title -->
+                        <h5 class="h5-sm txt-color-01">
+                            <a href="single-post.html">Quaerat neque purus ipsum neque dolor primis libero</a>
+                        </h5>
+
+                        <!-- Post Text -->
+                        <p class="txt-color-05">Quaerat neque purus ipsum neque dolor primis libero tempus impedit
+                            tempor at
+                            blandit sapien gravida donec ipsum undo porta justo...
+                        </p>
+
+                        <!-- Post Author -->
+                        <p class="post-author txt-color-05">Jane Smith - Sep 07, 2020</p>
+
+                    </div>
+
+                </div>
+            </div> <!-- END  BLOG POST #1 -->
+
+
+            <!-- BLOG POST #2 -->
+            <div class="col-md-6 col-lg-4">
+                <div class="blog-post">
+
+                    <!-- BLOG POST IMAGE -->
+                    <div class="blog-post-img">
+                        <img class="img-fluid"
+                            src="<?php echo get_template_directory_uri() . '/images/blog/img-02.jpg'; ?>"
+                            alt="blog-post-image" />
+                    </div>
+
+                    <!-- BLOG POST TEXT -->
+                    <div class="blog-post-txt">
+
+                        <!-- Post Data -->
+                        <span class="txt-color-06">Massage Therapy</span>
+
+                        <!-- Post Title -->
+                        <h5 class="h5-sm txt-color-01">
+                            <a href="single-post.html">Tempor blandit sapien at gravida donec ipsum</a>
+                        </h5>
+
+                        <!-- Post Text -->
+                        <p class="txt-color-05">Quaerat neque purus ipsum neque dolor primis libero tempus impedit
+                            tempor at
+                            blandit sapien gravida donec ipsum undo porta justo...
+                        </p>
+
+                        <!-- Post Author -->
+                        <p class="post-author txt-color-05">Wendy T. - Aug 31, 2020</p>
+
+                    </div>
+
+                </div>
+            </div> <!-- END  BLOG POST #2 -->
+
+
+            <!-- BLOG POST #3 -->
+            <div class="col-md-6 col-lg-4">
+                <div class="blog-post">
+
+                    <!-- BLOG POST IMAGE -->
+                    <div class="blog-post-img">
+                        <img class="img-fluid"
+                            src="<?php echo get_template_directory_uri() . '/images/blog/img-03.jpg'; ?>"
+                            alt="blog-post-image" />
+                    </div>
+
+                    <!-- BLOG POST TEXT -->
+                    <div class="blog-post-txt">
+
+                        <!-- Post Data -->
+                        <span class="txt-color-06">Aromatherapy</span>
+
+                        <!-- Post Title -->
+                        <h5 class="h5-sm txt-color-01"><a href="single-post.html">Neque dolor primis a libero tempus an
+                                impedit
+                                tempor</a>
+                        </h5>
+
+                        <!-- Post Text -->
+                        <p class="txt-color-05">Quaerat neque purus ipsum neque dolor primis libero tempus impedit
+                            tempor at
+                            blandit sapien gravida donec ipsum undo porta justo...
+                        </p>
+
+                        <!-- Post Author -->
+                        <p class="post-author txt-color-05">Stacey Richardson - Aug 19, 2020</p>
+
+                    </div>
+
+                </div>
+            </div> <!-- END  BLOG POST #3 -->
+
+
+        </div> <!-- END BLOG POSTS HOLDER -->
+
+
+    </div> <!-- End container -->
+</section> <!-- END BLOG-1 -->
