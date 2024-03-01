14fcfafd4097093ec55e9b1c56d097e4d117318d
isabelaranguren <isabelarangurenl01@gmail.com>
2024-03-01 13:31:15 -0600

    Delete unused elements

--- functions.php
+++ functions.php
@@ -161,7 +161,6 @@ function lash_extension_scripts() {
     wp_enqueue_script( 'jquery.scrollto', get_template_directory_uri() . '/js/jquery.scrollto.js', array(), null, true );
     wp_enqueue_script( 'menu', get_template_directory_uri() . '/js/menu.js', array(), null, true );
     wp_enqueue_script( 'materialize', get_template_directory_uri() . '/js/materialize.js', array(), null, true );
-    wp_enqueue_script( 'tweenmax', get_template_directory_uri() . '/js/tweenmax.min.js', array(), null, true );
     wp_enqueue_script( 'slideshow', get_template_directory_uri() . '/js/slideshow.js', array(), null, true );
     wp_enqueue_script( 'imagesloaded', get_template_directory_uri() . '/js/imagesloaded.pkgd.min.js', array(), null, true );
     wp_enqueue_script( 'isotope', get_template_directory_uri() . '/js/isotope.pkgd.min.js', array(), null, true );
@@ -169,16 +168,12 @@ function lash_extension_scripts() {
     wp_enqueue_script( 'owl.carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', array(), null, true );
     wp_enqueue_script( 'jquery.magnific-popup', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', array(), null, true );
     wp_enqueue_script( 'hero-form', get_template_directory_uri() . '/js/hero-form.js', array(), null, true );
-    wp_enqueue_script( 'contact-form', get_template_directory_uri() . '/js/contact-form.js', array(), null, true );
-    wp_enqueue_script( 'comment-form', get_template_directory_uri() . '/js/comment-form.js', array(), null, true );
-    wp_enqueue_script( 'booking-form', get_template_directory_uri() . '/js/booking-form.js', array(), null, true );
+    
     wp_enqueue_script( 'jquery.datetimepicker.full', get_template_directory_uri() . '/js/jquery.datetimepicker.full.js', array(), null, true );
     wp_enqueue_script( 'jquery.validate', get_template_directory_uri() . '/js/jquery.validate.min.js', array(), null, true );
-    wp_enqueue_script( 'jquery.ajaxchimp', get_template_directory_uri() . '/js/jquery.ajaxchimp.min.js', array(), null, true );
     wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array(), null, true );
     wp_enqueue_script( 'changer', get_template_directory_uri() . '/js/changer.js', array(), null, true );
     wp_enqueue_script( 'styleswitch', get_template_directory_uri() . '/js/styleswitch.js', array(), null, true );
-
 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
