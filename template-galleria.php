42ce4f9136d507570c0490ce31f5260c59380715
isabelaranguren <isabelarangurenl01@gmail.com>
2024-03-04 14:24:37 -0600

    Galleria

---
+++ template-galleria.php
@@ -0,0 +1,219 @@
+<?php
+
+/** 
+ * 
+ * Template Name: Galleria Location Page
+ * 
+ * 
+ */
+
+
+get_header();
+
+?>
+<?php get_template_part('template-parts/content', 'page'); ?>
+
+
+<section id="about-1" class="bg-color-01 wide-60 about-section division">
+    <div class="container txt-color-05">
+        <div class="row d-flex align-items-center">
+
+            <div class="col-md-7 col-lg-6">
+                <div class="txt-block left-column pc-15 mb-40">
+
+                    <!-- Title -->
+                    <p class="font-italic">Our Galleria location has proudly served the Greater Houston area since
+                        December of 2021. Using our years of expertise and experience, our team has helped many clients
+                        customize their lashes and brows to suit their facial structure and style preferences.
+                    </p>
+                    <p>Whether you want fuller, darker, or longer lashes or stylized brows, we'll help you achieve the
+                        look that you've envisioned for yourself. Schedule your free consultation now, and you'll meet
+                        one of our experienced lash technicians who can help you draft and design your new look.
+
+                    </p>
+                    <ul class="text-left p-4  tracking-wider">
+                        <li class="list-item">Each of our lash stylists has years of experience, certifications, and
+                            training behind them. Each member of our team has a different area of expertise. Some have
+                            better hands for grafting fuller lash extensions; others can show off their magic when
+                            they're focused on grafting natural lashes. No matter what you're looking for, we have the
+                            lash technician to help you achieve the look and style you want.</li><br />
+                        <li class="list-item">To ensure that each of our visiting clients have a dedicated technician
+                            during their appointment, our services are available by appointment only. Each of our
+                            clients has their own circumstances, conditions, and worries. Some visit us with back pain
+                            or chronic conditions that prevent them from staying in one position for too long, are
+                            pregnant, have claustrophobia, or need further accommodation to ensure each one gets the
+                            lashes or brows they want. After all, every client is God's darling and deserves to feel
+                            their best by looking their best..</li>
+                    </ul>
+
+
+                </div>
+            </div> <!-- END TEXT BLOCK -->
+            <!-- IMAGE BLOCK -->
+            <div class="col-md-5 col-lg-6">
+                <div class="left-column pc-10 mb-40">
+                    <h5 class="h5-sm txt-color-01">Address</h5>
+
+                    <div class="d-flex align-items-center mb-3">
+                        <i class="fa-solid fa-map-location"></i>
+                        <p class="mb-0 ml-4">2400 Augusta Dr Suite #110 Houston, TX 77057</p>
+                    </div>
+
+                    <div style="cursor:pointer;" class="d-flex align-items-center mb-3">
+                        <i class="fa-solid fa-phone-volume" style="color: red;"></i>
+                        <p class="mb-0 ml-4">
+                            (713)-392-0281</p>
+                    </div>
+
+                    <div class="post-share-list">
+                        <ul class="list-unstyled d-flex justify-content-start">
+                            <li class="mr-3">
+                                <a href="https://www.facebook.com/ilashfiona" class="share-ico ico-facebook"><i
+                                        class="fab fa-facebook-square"></i></a>
+                            </li>
+                            <li class="mr-3">
+                                <a href="https://www.tiktok.com/@lashextension.by" class="share-ico ico-tiktok"><i
+                                        class="fab fa-tiktok"></i></a>
+                            </li>
+                            <li class="mr-3">
+                                <a href="https://www.instagram.com/lashextensionbyfiona_galleria/"
+                                    class="share-ico ico-instagram"><i class="fab fa-instagram"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <h5 class="h5-sm txt-color-01 pt-30">Our Hours</h5>
+                    <div>
+                        <table class="table txt-color-05">
+                            <tbody>
+                                <tr>
+                                    <td>Monday</td>
+                                    <td> - </td>
+                                    <td class="text-right">8:00 AM - 9:00 PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Tuesday</td>
+                                    <td> - </td>
+                                    <td class="text-right">8:00 AM - 9:00 PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Wednesday</td>
+                                    <td> - </td>
+                                    <td class="text-right">8:00 AM - 9:00 PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Thursday</td>
+                                    <td> - </td>
+                                    <td class="text-right">8:00 AM - 9:00 PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Friday</td>
+                                    <td> - </td>
+                                    <td class="text-right">8:00 AM - 9:00 PM</td>
+                                </tr>
+                                <tr class="last-tr">
+                                    <td>Saturday</td>
+                                    <td> - </td>
+                                    <td class="text-right">9:00 AM - 7:30 PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Sunday</td>
+                                    <td> - </td>
+                                    <td class="text-right">9:00 AM - 7:30 PM</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div> <!-- End row -->
+    </div> <!-- End container -->
+</section> <!-- END ABOUT-1 -->
+<section class="">
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d221947.2863494473!2d-95.5524262!3d29.6348823!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640e67142272cd5%3A0x5ec332cec0a26b38!2s4131%20Hwy%206%2C%20Sugar%20Land%2C%20TX%2077478!5e0!3m2!1sen!2sus!4v1709578706052!5m2!1sen!2sus"
+        width="100%" height="450" style="border:0;" allowfullscreen loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <section>
+        <section id="about-1" class="bg-color-01 wide-60 about-section division">
+            <div class="container">
+                <div class="row d-flex align-items-center">
+                    <!-- IMAGE BLOCK -->
+                    <div class="col-md-5 col-lg-6">
+                        <div class="img-block left-column pc-10 mb-40">
+
+                            <img class="img-fluid"
+                                src="<?php echo get_template_directory_uri() . '/images/image-01.png'; ?>"
+                                alt="about-image">
+                        </div>
+                    </div>
+                    <div class="col-md-7 col-lg-6">
+                        <div class="txt-block right-column pc-15 mb-40">
+                            <!-- Title -->
+                            <span class="txt-color-06 bolder ">Why choose us? </span>
+                            <h2 class="h3-md txt-color-01">Why should you visit our Galleria location?</h2>
+                            <!-- Text -->
+                            <p class="txt-color-05 custom-height p-lg">When you're looking for a customized experience
+                                and look, you go to the professionals. And what better professional is there than one
+                                that looks after you? No matter what reason you want to change your lashes or brows, our
+                                Galleria location has the experience, medical-grade equipment, and space to help you
+                                achieve the effect you're looking for.</p>
+
+
+                            <a href="https://www.fresha.com/providers/ilash-health-spa-u92j25pw?dppub=true"
+                                class="btn btn-md btn-color-02 color-01-hover">Make an Appointment</a>
+                        </div>
+                    </div> <!-- END TEXT BLOCK -->
+                </div> <!-- End row -->
+            </div> <!-- End container -->
+        </section> <!-- END ABOUT-1 -->
+        <section id="about-1" class="bg-color-01 about-section division wide-60">
+            <div class="container">
+                <div class="row d-flex align-items-center">
+                    <!-- TEXT BLOCK -->
+                    <div class="col-md-7 col-lg-6">
+                        <div class="txt-block right-column pc-15 mb-40">
+
+                            <!-- Title -->
+                            <span class="txt-color-06 bolder">About our studio</span>
+                            <h3 class="h3-md txt-color-01">What does our Galleria location offer?
+                            </h3>
+                            <!-- Text -->
+                            <p class="txt-color-05 custom-height p-lg">Of course, we have nothing but the best for all
+                                of our clients.
+                            </p>
+
+
+                            <p class="txt-color-05">When you schedule your free consultation, you'll speak with one of
+                                our experienced lash stylists, who will help you decide on a design and style for your
+                                lash or brows.
+
+                            </p>
+                            <p class="txt-color-05">We'll customize our services to match your preferences and facial
+                                structure, so no matter what your choices will bring out the best in you.
+
+                            </p>
+                            <p class="txt-color-05">You can choose between lash extensions, lash tinting, eyebrow
+                                waxing, and microblading to get started. Everyone deserves to achieve their best look,
+                                and our team would love to help bring out your inner beauty!
+                        </div>
+
+                    </div> <!-- END TEXT BLOCK -->
+                    <!-- IMAGE BLOCK -->
+                    <div class="col-md-5 col-lg-6">
+                        <div class="img-block left-column pc-10 mb-40">
+                            <img class="img-fluid"
+                                src="<?php echo get_template_directory_uri() . '/images/collage-03.png'; ?>"
+                                alt="about-image">
+                        </div>
+                    </div>
+
+                </div> <!-- End row -->
+            </div> <!-- End container -->
+        </section> <!-- END ABOUT-1 -->
+        <!-- BANNER-5 ============================================= -->
+        <?php get_template_part( 'template-parts/content', 'banner' ); ?>
+
+
+        <?php get_footer(); ?>
