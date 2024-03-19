0120543039f679d1afffeb907ccd6149ed02f0dd
isabelaranguren <isabelarangurenl01@gmail.com>
2024-03-19 14:04:01 -0500

    Footer Redesign

--- footer.php
+++ footer.php
@@ -1,118 +1,149 @@
-<!-- FOOTER--->
 <footer id="footer-4" class="bg-color-01 footer division">
     <div class="container">
         <!-- FOOTER CONTENT -->
         <div class="row">
             <!-- FOOTER INFO -->
-            <div class="col-md-5 col-lg-4">
-                <div class="footer-info mb-20">
-                    <!-- For Retina Ready displays take a image with double the amount of pixels that your image will be 
-									displayed (e.g 416 x 120  pixels) -->
-                    <img src="<?php echo get_template_directory_uri() . '/images/logo-01.png'; ?>" alt="logo"
-                        width="204">
-                    <p class="txt-color-05 mt-20">Enjoy a semi-permanent curling treatment that enhances the shape of
-                        your lashes, making your eyes appear more open and youthful.</p>
-                    <div class="txt-color-05">
-                        <p class="txt-color-05 mt-40 bold-title">Sugar Land Location</p>
-                        <p class=""><i class="fa-solid fa-location-dot">&nbsp;</i> 4131 Highway 6 Suite #9 Sugar Land,
-                            77478</p>
-                        <p class="foo-phone"><i class="fa-solid fa-phone"> &nbsp;</i><a href="tel:+1(713)-392-0281">
-                                +1(713) 392-0281</a></p>
-
-                        <p class="txt-color-05 mt-40 bold-title">Galleria Location</p>
-                        <!-- Email -->
-                        <p><i class="fa-solid fa-location-dot">&nbsp;</i> 2400 Augusta Dr Suite #110 Houston, TX 77057
-                        <p class="foo-phone"><i class="fa-solid fa-envelope"> &nbsp;</i> <a
-                                href="mailto:fiona160715@yahoo.com">fiona160715@yahoo.com</a>
-                            <!-- Phone -->
-                        <p class="foo-phone"><i class="fa-solid fa-phone"> &nbsp;</i><a
-                                href="tel:+1(281)-690-3563">+1(281) 690-3563</a></p>
-
-                    </div>
-                </div>
-            </div>
             <div class="col-md-12 col-lg-3">
-                <div class="footer-img mb-40">
-                    <!-- Title -->
-                    <h6 class="h6-lg txt-color-01">Our Hours</h6>
-                    <!-- Instagram Images -->
-                    <table class="table txt-color-05">
+                <div class="footer-info mb-20">
+                    <h6 class="h6-lg txt-color-01 ">Sugar Land - Location</h6>
+                    <p class="txt-color-05"><i class="fa-solid fa-location-dot">&nbsp;</i> 4131 Highway 6 Suite #9 Sugar
+                        Land,
+                        77478</p>
+                    <p class="foo-phone txt-color-05"><i class="fa-solid fa-phone">&nbsp;</i><a
+                            href="tel:+1(713)-392-0281">+1(713)
+                            392-0281</a></p>
+                    <p class="foo-mail txt-color-05"><a
+                            href="mailto:info@lashextensionsbyfiona.com">info@lashextensionsbyfiona.com</a>
+
+                    <table class="footer-table txt-color-05">
                         <tbody>
                             <tr>
                                 <td>Monday</td>
-                                <td> - </td>
+                                <td></td>
                                 <td class="text-right">8:00 AM 9:00 PM</td>
                             </tr>
                             <tr>
                                 <td>Tuesday</td>
-                                <td> - </td>
+                                <td></td>
                                 <td class="text-right">8:00 AM 9:00 PM</td>
                             </tr>
                             <tr>
                                 <td>Wednesday</td>
-                                <td> - </td>
+                                <td></td>
                                 <td class="text-right">8:00 AM 9:00 PM</td>
                             </tr>
                             <tr>
                                 <td>Thursday</td>
-                                <td> - </td>
+                                <td></td>
                                 <td class="text-right">8:00 AM 9:00 PM</td>
                             </tr>
                             <tr>
                                 <td>Friday</td>
-                                <td> - </td>
+                                <td></td>
                                 <td class="text-right">8:00 AM 9:00 PM</td>
                             </tr>
                             <tr class="last-tr">
                                 <td>Saturday Sunday</td>
-                                <td> - </td>
+                                <td></td>
                                 <td class="text-right">9:00 AM 7:30 PM</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+            </div>
+            <div class="col-md-12 col-lg-3">
+                <div class="footer-img mb-20">
+                    <!-- Title -->
+                    <h6 class="h6-lg txt-color-01">Galleria - Location</h6>
+                    <!-- Email -->
+                    <p class="txt-color-05"><i class="fa-solid fa-location-dot">&nbsp;</i> 2400 Augusta Dr Suite #110
+                        Houston, TX 77057
+                    <p class="foo-phone txt-color-05"><i class="fa-solid fa-phone"> &nbsp;</i><a
+                            href="tel:+1(281)-690-3563">+1(281)
+                            690-3563</a></p>
+                    <p class="foo-phone txt-color-05"><a
+                            href="mailto:info@lashextensionsbyfiona.com">info@lashextensionsbyfiona.com</a>
+                        <!-- Phone -->
+
+                    <table class="footer-table txt-color-05">
+                        <tbody>
+                            <tr>
+                                <td>Monday</td>
+                                <td></td>
+                                <td class="text-right">9:00 AM 7:00 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Tuesday</td>
+                                <td></td>
+                                <td class="text-right">9:00 AM 7:00 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Wednesday</td>
+                                <td></td>
+                                <td class="text-right">9:00 AM 7:00 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Thursday</td>
+                                <td></td>
+                                <td class="text-right">9:00 AM 7:00 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Friday</td>
+                                <td></td>
+                                <td class="text-right">9:00 AM 7:00 PM</td>
+                            </tr>
+                            <tr class="last-tr">
+                                <td>Saturday Sunday</td>
+                                <td></td>
+                                <td class="text-right">Closed</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <!-- Instagram Images -->
+                </div>
             </div> <!-- END FOOTER IMAGES -->
             <!-- FOOTER LINKS -->
             <!-- Services -->
             <div class="col-md-4 col-lg-3 col-xl-3">
-                <div class="footer-contacts mb-40 ml-40">
+                <div class="footer-contacts mb-20 ml-20">
                     <!-- Title -->
                     <h6 class="h6-lg txt-color-01 ">Services</h6>
                     <!-- Services -->
-                    <a href="https://lashextensionsbyfiona.com/services/lash-extensions/">
+                    <a href="/services/lash-extensions/">
                         <p class="txt-color-05 p-2 ">Lash Extensions</p>
                     </a>
-                    <a href="https://lashextensionsbyfiona.com/services/microblading/">
+                    <a href="/services/microblading/">
                         <p class="txt-color-05 p-2 ">Microblading</p>
                     </a>
-                    <a href="https://lashextensionsbyfiona.com/services/eyebrow-waxing/">
+                    <a href="/services/eyebrow-waxing/">
                         <p class="txt-color-05 p-2 ">Eyebrow Waxing</p>
                     </a>
-                    <a href="https://lashextensionsbyfiona.com/services/lash-tinting/">
+                    <a href="/services/lash-tinting/">
                         <p class="txt-color-05 p-2 ">Lash Tinting</p>
+                    </a>
+                    <h6 class="h6-lg txt-color-01 mt-20">Quick Links</h6>
+                    <a href="/sitemap/">
+                        <p class="txt-color-05 p-2">Site Map</p>
+                    </a>
+                    <a href="/contact-us/">
+                        <p class="txt-color-05 p-2">Contact</p>
                     </a>
                 </div>
             </div>
-            <div class="col-md-3 col-lg-2">
-                <div class="footer-links mb-40">
-                    <!-- Title -->
-                    <h6 class="h6-lg txt-color-01 ">Quick Links</h6>
-                    <!-- Footer Links -->
-                    <ul class="clearfix txt-color-05  ">
-                        <?php wp_nav_menu(array('menu_class' => 'clearfix txt-color-05 ', 'container' => 'ul',)); ?>
-
-                    </ul>
+            <div class="col-md-4 col-lg-3 col-xl-3">
+                <div class="footer-info mb-20">
+                    <!-- For Retina Ready displays take a image with double the amount of pixels that your image will be 
+                                    displayed (e.g 416 x 120  pixels) -->
+                    <img src="<?php echo get_template_directory_uri() . '/images/logo-01.png'; ?>" alt="logo"
+                        width="204">
+                    <p class="txt-color-05 mt-20">Enjoy a semi-permanent curling treatment that enhances the shape of
+                        your lashes, making your eyes appear more open and youthful.</p>
+                    <a href="https://www.fresha.com/providers/ilash-health-spa-u92j25pw?dppub=true"
+                        class="btn btn-md btn-color-01">Book an Appointment</a>
                 </div>
             </div>
 
-            <div class="col-md-8 col-lg-4 col-xl-3">
-                <div class="gmap ">
-                    <iframe title="gmap"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235098.09685553284!2d-95.72135181717756!3d29.654948458407556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640e67142272cd5%3A0x5ec332cec0a26b38!2s4131%20Hwy%206%2C%20Sugar%20Land%2C%20TX%2077478!5e0!3m2!1sen!2sus!4v1709654764264!5m2!1sen!2sus"
-                        width="100%" height="240" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
         </div> <!-- END FOOTER CONTENT -->
         <!-- BOTTOM FOOTER -->
         <div class="bottom-footer txt-color-05">
@@ -132,7 +163,7 @@
                                     Facebook</a></p>
                         </li>
                         <li>
-                            <p><a href="https://www.instagram.com/lashextensionbyfiona_galleria"><i
+                            <p><a href="https://www.instagram.com/lashextensionbyfiona_tableia"><i
                                         class="fab fa-instagram"></i> Instagram</a></p>
                         </li>
                         <li>
