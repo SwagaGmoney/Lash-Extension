3b1666f25b76dd7646fdf9b3459bf0b244fa34bd
isabelaranguren <isabelarangurenl01@gmail.com>
2024-04-08 14:27:57 -0500

    WIP

---
+++ template-gallery.php
@@ -0,0 +1,276 @@
+<?php
+
+/** 
+ * 
+ * Template Name: Gallery Photo Page
+ * 
+ */
+
+
+get_header();
+
+get_template_part('template-parts/content', 'page'); ?>
+
+<section id="gallery-1" class="bg-color-01 wide-80 gallery-section division">
+    <div class="container">
+        <!-- SECTION TITLE -->
+
+        <!-- GALLERY FILTERING BUTTONS -->
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="masonry-filter mb-40">
+                    <button data-filter="*" class="is-checked">All</button>
+                    <button data-filter=".spa">Other Services</button>
+                    <button data-filter=".massage">Classic lashes</button>
+                    <button data-filter=".aroma">YY Lash</button>
+                    <button data-filter=".herbs">Microblading</button>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- GALLERY IMAGES HOLDER -->
+        <div class="row">
+            <div class="col-md-12 gallery-items-list">
+                <div class="masonry-wrap grid-loaded">
+
+                    <!-- IMAGE #1 -->
+                    <div class="masonry-item spa massage">
+                        <div class="hover-overlay">
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-1.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-1.jpg">Classic
+                                            Lashes</a></h5>
+
+
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #1 -->
+
+
+                    <!-- IMAGE #2 -->
+                    <div class="masonry-item spa">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-09.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link  -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-09.jpg">Relaxing
+                                            Massage</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>SPA & Wellness</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #2 -->
+
+
+                    <!-- IMAGE #3 -->
+                    <div class="masonry-item aroma">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-2.jpg" alt="gallery-imagee" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-2.jpg">Supreme
+                                            Skincare</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>Aroma & Skin Care</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #3 -->
+
+
+                    <!-- IMAGE #4 -->
+                    <div class="masonry-item spa">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-5.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-5.jpg">Hot Stones
+                                            Therapy</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>SPA & Aromatherapy</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #4 -->
+
+
+                    <!-- IMAGE #5 -->
+                    <div class="masonry-item massage herbs">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-3.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-3.jpg">Thai
+                                            Massage</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>Herbs & Skin Care</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #5 -->
+
+
+                    <!-- IMAGE #6 -->
+                    <div class="masonry-item herbs">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-4.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-4.jpg">Relaxing
+                                            Massage</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>Herbs & Massage Therapy</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #6 -->
+
+
+                    <!-- IMAGE #7 -->
+                    <div class="masonry-item aroma herbs">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-10.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-10.jpg">Facial
+                                            Spa</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>Herbs & Skin Care</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #7 -->
+
+
+                    <!-- IMAGE #8 -->
+                    <div class="masonry-item aroma">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-11.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-11.jpg">Scrub
+                                            Massage</a></h5>
+
+                                    <!-- Image Meta -->
+                                    <p>Aroma & Massage Therapy</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #8 -->
+
+
+                    <!-- IMAGE #9 -->
+                    <div class="masonry-item spa massage">
+                        <div class="hover-overlay">
+
+                            <!-- Image -->
+                            <img class="img-fluid" src="images/gallery/img-6.jpg" alt="gallery-image" />
+                            <div class="item-overlay"></div>
+
+                            <!-- Image Description -->
+                            <div class="image-description white-color">
+                                <div class="image-data">
+
+                                    <!-- Image Link -->
+                                    <h5 class="h5-sm"><a class="image-link" href="images/gallery/img-6.jpg">Deep Tissue
+                                            Massage</a></h5>
+                                    <!-- Image Meta -->
+                                    <p>SPA & Massage Therapy</p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- END IMAGE #9 -->
+
+
+                </div>
+            </div>
+        </div> <!-- END GALLERY IMAGES HOLDER -->
+
+
+    </div> <!-- End container -->
+</section> <!-- END GALLERY-1 -->
+
+<?php get_footer(); ?>
